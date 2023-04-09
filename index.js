d222f4d1ac5dae24b1fb1f0e34bf3be3eea381a5
edopore <ediij1994@gmail.com>
2023-04-08 21:53:39 -0500

    ADD: Setting up

---
+++ index.js
@@ -0,0 +1,11 @@
+const container = require("./src/startup/container");
+const server = container.resolve("app");
+const {MONGO_URI} = container.resolve("config");
+
+const mongoose = require("mongoose");
+//mongoose.set("useUnifiedTopology",true);
+
+mongoose.connect(MONGO_URI,{}).
+then(() => {
+    server.start()
+}).catch(console.log)
---
+++ src/controllers/home.controller.js
@@ -0,0 +1,13 @@
+let _homeService = null;
+
+class HomeController{
+    constructor({HomeService}){
+        _homeService = HomeService;
+    }
+
+    index(request,response){
+        return response.send(_homeService.index());
+    }
+}
+
+module.exports = HomeController
